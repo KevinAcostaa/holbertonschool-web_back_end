90dccabfec0d3e1fb3cee19e31e60d2953aeccee
kevinacostaa <kevinmag999@gmail.com>
2025-03-30 21:42:33 -0300

    1-make_classrooms.js

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,13 @@
+import ClassRoom from './0-classroom';
+
+export function initializeRooms(){
+    const rooms = [];
+
+    ClassRoom.push(new ClassRoom(19));
+    ClassRoom.push(new ClassRoom(20));
+    ClassRoom.push(new ClassRoom(34));
+
+    return rooms;
+}
+
+export default initializeRooms;
