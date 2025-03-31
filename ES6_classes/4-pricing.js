ba8f89c3a56864d0cb20eea20603d99fcfd09ea5
kevinacostaa <kevinmag999@gmail.com>
2025-03-30 23:06:42 -0300

    4-pricing.js

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,47 @@
+import Currency from './3-currency';
+
+class Pricing {
+    constructor(amount, currency) {
+        if (typeof amount !== 'number') {
+            throw new TypeError('amount must be a number');
+        }
+
+        if (!(currency instanceof Currency)) {
+            throw Error('The currency parameter must be an instance of the Currency class');
+        }
+
+        this._amount = amount;
+        this._currency = currency;
+    }
+
+    get amount() {
+        return this._amount;
+    }
+
+    set amount(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('amount must be a number');
+        }
+        this._amount = value;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+
+    set currency(value) {
+        if (!(value instanceof Currency)) {
+            throw Error('The currency parameter must be an instance of the Currency class');
+        }
+        this._currency = value;
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+    }
+
+    static convertPrice(amount, conversionRate) {
+        return amount * conversionRate;
+    }
+}
+export default Pricing;
