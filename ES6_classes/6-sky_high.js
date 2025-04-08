951f4d40dc4f15b404fc88cfd5b9361b0b33c887
kevinacostaa <kevinmag999@gmail.com>
2025-04-08 15:55:11 -0300

    6-sky_high.js

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,10 +1,9 @@
 import Building from './5-building.js';
 
 class SkyHighBuilding extends Building {
-
     constructor(sqft, floors) {
         super(sqft);
-        this.floors = floors;
+        this._floors = floors;
     }
 
 
@@ -12,7 +11,7 @@ class SkyHighBuilding extends Building {
         return this._floors;
     }
 
-    evacuationWarningMessage(){
+    evacuationWarningMessage() {
         return `Evacuate slowly the ${this._floors} floors`;
     }
 }
