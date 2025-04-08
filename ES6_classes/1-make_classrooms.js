2f6d229824379232a26f1b004dfde7e30e7b10e8
kevinacostaa <kevinmag999@gmail.com>
2025-04-08 15:28:10 -0300

    eslint

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,13 +1,13 @@
 import ClassRoom from './0-classroom';
 
-export function initializeRooms(){
-    const rooms = [];
+export function initializeRooms() {
+  const rooms = [];
 
-    rooms.push(new ClassRoom(19));
-    rooms.push(new ClassRoom(20));
-    rooms.push(new ClassRoom(34));
+  rooms.push(new ClassRoom(19));
+  rooms.push(new ClassRoom(20));
+  rooms.push(new ClassRoom(34));
 
-    return rooms;
+  return rooms;
 }
 
 export default initializeRooms;
