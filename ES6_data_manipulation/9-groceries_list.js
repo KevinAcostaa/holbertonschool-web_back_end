263f78782ae9be5e870d77a9c94f8839917ad5f9
kevinacostaa <kevinmag999@gmail.com>
2025-04-13 20:26:05 -0300

    9-groceries_list.js

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,12 +1,11 @@
-
 export default function groceriesList() {
-    const names = ['Apples', 'Tomatoes', 'Pasta', 'Rice', 'Banana'];
-    const quantities = [10, 10, 1, 1, 5];
-    const My_list = new Map()
+  const names = ['Apples', 'Tomatoes', 'Pasta', 'Rice', 'Banana'];
+  const quantities = [10, 10, 1, 1, 5];
+  const My_list = new Map();
 
-    for (let i = 0; i < names.length; i += 1){
-        My_list.set(names[i], quantities[i]);
-    }
+  for (let i = 0; i < names.length; i += 1) {
+    My_list.set(names[i], quantities[i]);
+  }
 
-    return My_list;
+  return My_list;
 }
