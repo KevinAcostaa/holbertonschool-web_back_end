8554028d54b9dd9fef9bd8feaa31cb7fcfdaec66
kevinacostaa <kevinmag999@gmail.com>
2025-04-25 19:26:34 -0300

    2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+this module returns the floor of the float.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    returns the floor of the float.
+    """
+    return math.floor(n)
