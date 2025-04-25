23421c24ffe09435cd09d972e084c954fa397328
kevinacostaa <kevinmag999@gmail.com>
2025-04-25 19:31:13 -0300

    3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module converts a float to a string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This module converts a float to a string
+    """
+    return str(n)
