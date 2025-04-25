4426444d1d206c131dfde9514b78a47d0ca3ddec
kevinacostaa <kevinmag999@gmail.com>
2025-04-25 19:17:19 -0300

    1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+this module concat two strings and return an string concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    this module concat two strings and return an string concat
+    """
+    return str1 + str2
