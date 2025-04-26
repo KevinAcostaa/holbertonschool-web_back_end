608b1be32ca9d0799c1eff8214bc5d7eeead1fbc
kevinacostaa <kevinmag999@gmail.com>
2025-04-26 15:22:55 -0300

    8-make_multiplier.py

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 """
-
+function that takes a float as an argument and returns
+a function that multiplies a float by multiplier
 """
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
-    
+    function that takes a float as an argument and returns
+    a function that multiplies a float by multiplier
     """
     def multiply(num: float) -> float:
         return num * multiplier
