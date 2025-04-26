b207545c772d113b59b207a7b9d93e67156d7d4d
kevinacostaa <kevinmag999@gmail.com>
2025-04-26 15:28:04 -0300

    9-element_length.py

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,5 +1,12 @@
+#!/usr/bin/env python3
+"""
+function parameters and return values with the appropriate types
+"""
 from typing import Iterable, Tuple, List, Any
 
 
 def element_length(lst: Iterable[Any]) -> List[Tuple[Any, int]]:
+    """
+    function parameters and return values with the appropriate types
+    """
     return [(i, len(i)) for i in lst]
