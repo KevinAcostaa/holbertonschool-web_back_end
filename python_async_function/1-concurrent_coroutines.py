fbb9c779f3a9b5b8512889cd4d67b430edb9c6dd
kevinacostaa <kevinmag999@gmail.com>
2025-04-28 22:52:39 -0300

    1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,7 @@ this module contains a function
 
 import asyncio
 from typing import List
-wait_random = __import__("0-basic_async_syntax.py").wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
