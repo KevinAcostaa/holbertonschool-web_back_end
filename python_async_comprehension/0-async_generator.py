887f2bf1a10a08a6424413de50335c7426359678
kevinacostaa <kevinmag999@gmail.com>
2025-05-02 20:05:38 -0300

    0-async_generator.py

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+this module contains a function
+"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    this function return num random between 0 and 10
+    """
+    async for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
